c427a3de0a833d6728255a0be93c0403fc1516ba
stxalth <azz.malez@gmail.com>
2022-05-15 16:31:18 +0700

    Added

--- components/Post.js
+++ components/Post.js
@@ -15,9 +15,14 @@ function Post({ key, name, message, email, timestamp, image, postImage }) {
           />
           <div>
             <p className="font-medium">{name}</p>
-            <p className="text-xs text-gray-400">
-              {new Date(timestamp?.toDate()).toLocaleString()}
-            </p>
+
+            {timestamp ? (
+              <p className="text-xs text-gray-400">
+                {new Date(timestamp?.toDate()).toLocaleString()}
+              </p>
+            ) : (
+              <p className="text-xs text-gray-400">Loading</p>
+            )}
           </div>
         </div>
         <p className="pt-4">{message}</p>
